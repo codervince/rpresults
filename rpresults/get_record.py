c00d642ba04c19923fe301061c79fb9a008d01ae
python273 <python273@yandex.ru>
2016-02-19 17:55:02 +0300

    Get record example

---
+++ rpresults/get_record.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pymongo
+from settings import MONGO_DBNAME
+
+raceid = "616064"
+horseid = "838250"
+
+mongo = pymongo.MongoClient()
+db = mongo[MONGO_DBNAME]
+
+result = db.results.find_one({'raceid': raceid, 'horseid': horseid})
+
+trainer = db.trainers.find_one({'trainerid': result['trainerid']})
+jockey = db.jockeys.find_one({'jockeyid': result['jockeyid']})
+
+race = db.races.find_one({'raceid': result['raceid']})
+venue = db.venues.find_one({'venueid': race['venueid']})
+
+horse = db.horses.find_one({'horseid': result['horseid']})
+
+print 'result', result, '\n'
+print 'trainer', trainer, '\n'
+print 'jockey', jockey, '\n'
+print 'race', race, '\n'
+print 'venue', venue, '\n'
+print 'horse', horse, '\n'
