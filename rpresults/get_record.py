3012af9d12e7000d2447b9730b5a5ecdd1e13066
python273 <python273@yandex.ru>
2016-02-19 17:57:39 +0300

    Get record example

--- rpresults/get_record.py
+++ rpresults/get_record.py
@@ -10,17 +10,20 @@ db = mongo[MONGO_DBNAME]
 
 result = db.results.find_one({'raceid': raceid, 'horseid': horseid})
 
-trainer = db.trainers.find_one({'trainerid': result['trainerid']})
-jockey = db.jockeys.find_one({'jockeyid': result['jockeyid']})
+if result is None:
+    print 'record not found'
+else:
+    trainer = db.trainers.find_one({'trainerid': result['trainerid']})
+    jockey = db.jockeys.find_one({'jockeyid': result['jockeyid']})
 
-race = db.races.find_one({'raceid': result['raceid']})
-venue = db.venues.find_one({'venueid': race['venueid']})
+    race = db.races.find_one({'raceid': result['raceid']})
+    venue = db.venues.find_one({'venueid': race['venueid']})
 
-horse = db.horses.find_one({'horseid': result['horseid']})
+    horse = db.horses.find_one({'horseid': result['horseid']})
 
-print 'result', result, '\n'
-print 'trainer', trainer, '\n'
-print 'jockey', jockey, '\n'
-print 'race', race, '\n'
-print 'venue', venue, '\n'
-print 'horse', horse, '\n'
+    print 'result', result, '\n'
+    print 'trainer', trainer, '\n'
+    print 'jockey', jockey, '\n'
+    print 'race', race, '\n'
+    print 'venue', venue, '\n'
+    print 'horse', horse
